63565077a44db0705a7f259a441793b453e086d6
Steven Paustian <steven.paustian@jackrabbitsystems.com>
2015-03-17 16:25:00 -0600

    added fragment to contain sudoku board

---
+++ SudokuSolver/app/src/main/java/edu/nmt/cse326/sudokusolver/BoardFragment.java
@@ -0,0 +1,38 @@
+package edu.nmt.cse326.sudokusolver;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+import com.google.android.gms.plus.PlusOneButton;
+
+/*
+
+This fragment will contain the 9x9 board representing the puzzle
+
+
+*/
+public class BoardFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup parent,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_board, parent, false);
+
+        return view;
+    }
+
+
+
+}
