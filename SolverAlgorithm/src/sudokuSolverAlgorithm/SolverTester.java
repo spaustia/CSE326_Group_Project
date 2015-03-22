176a5a481605a08bf17e999c91d2c78f1ebe6983
Sean Salinas <naeslas@gmail.com>
2015-03-22 12:18:52 -0600

    Started writing the SudokuSolver algorithm

---
+++ SolverAlgorithm/src/sudokuSolverAlgorithm/SolverTester.java
@@ -0,0 +1,13 @@
+
+package sudokuSolverAlgorithm;
+
+public class SolverTester
+{
+
+	public static void main(String[] args)
+	{
+		System.out.println("Sudoku Solver Algorithm Tester");
+
+	}
+
+}
---
+++ SolverAlgorithm/src/sudokuSolverAlgorithm/SudokuSolver.java
@@ -0,0 +1,19 @@
+
+package sudokuSolverAlgorithm;
+
+public class SudokuSolver
+{
+	private int[] input;
+	private int[] solution;
+
+	public SudokuSolver(int[] input)
+	{
+		this.input = input;
+	}
+
+	public int[] solve()
+	{
+
+		return solution;
+	}
+}
