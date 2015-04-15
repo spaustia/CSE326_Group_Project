9c636b482daa71d9b64803a08b6ac4333e2c110a
Steven Paustian <steven.paustian@jackrabbitsystems.com>
2015-04-14 20:24:28 -0600

    removed action bar

--- SudokuSolver/app/src/main/java/edu/nmt/cse326/sudokusolver/MainActivity.java
+++ SudokuSolver/app/src/main/java/edu/nmt/cse326/sudokusolver/MainActivity.java
@@ -12,13 +12,15 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+//        getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
