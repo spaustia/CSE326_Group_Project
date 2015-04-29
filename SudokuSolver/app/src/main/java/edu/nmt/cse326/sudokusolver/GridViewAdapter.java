5342a6c550a0514e5abc0409a2422c4dbe28c7e3
Steven Paustian <steven.paustian@jackrabbitsystems.com>
2015-04-29 16:43:47 -0600

    added javadoc comments for GridViewAdapter

--- SudokuSolver/app/src/main/java/edu/nmt/cse326/sudokusolver/GridViewAdapter.java
+++ SudokuSolver/app/src/main/java/edu/nmt/cse326/sudokusolver/GridViewAdapter.java
@@ -23,6 +23,12 @@ public class GridViewAdapter extends BaseAdapter
 	private ArrayAdapter <CharSequence> adapter;
 	private AdapterView.OnItemSelectedListener mListener;
 
+    /**
+     * Constructor for the adapter.
+     *
+     * @param c The Current Context
+     * @param listener The listen that should be used for spinner selection
+     */
 	public GridViewAdapter(Context c, AdapterView.OnItemSelectedListener listener)
 	{
 		mContext = c;
@@ -30,24 +36,42 @@ public class GridViewAdapter extends BaseAdapter
 		mListener = listener;
 		createSpinAdapter();
 	}
-
+    /**
+     * Returns the total number of view objects
+     *
+     */
 	public int getCount()
 	{
 		return 81;
 	}
 
+    /**
+     * Return the object at position
+     *
+     * @param position return the item at the given position
+     */
 	public Object getItem(int position)
 	{
 		return mSpinners[position];
 	}
 
+    /**
+     *  Get the id of the object at position
+     *
+     * @param position return the id at the given position
+     */
 	public long getItemId(int position)
 	{
 		return 0;
 	}
 
-	// create a new ImageView for each item referenced by the Adapter
-	public View getView(int position, View convertView, ViewGroup parent)
+    /**
+     *  Populate and return a spinner view
+     *
+     * @param position return a view for the given position
+     * @param convertView view to be converted
+     * @param parent the ViewGroup parent child to be inserted too
+     */	public View getView(int position, View convertView, ViewGroup parent)
 	{
 		final Spinner mSpinner;
 
@@ -87,8 +111,13 @@ public class GridViewAdapter extends BaseAdapter
 		return mSpinner;
 	}
 
-    // Set spinner borders for clean board
-	private void setSpinnerForPosition(Spinner spinner, int position)
+    /**
+     *  Set the spinner boards for clean game board
+     *
+     * @param position position of the spinner
+     * @param spinner the spinner we are setting the border for
+     */
+    private void setSpinnerForPosition(Spinner spinner, int position)
 	{
 		if (position % 3 == 0)
 		{
@@ -120,8 +149,12 @@ public class GridViewAdapter extends BaseAdapter
 
 	}
 
-	// Setup and return an ArrayAdapter for a spinner
-	private void createSpinAdapter()
+
+    /**
+     *  Create the adapter for the spinners
+     *
+     */
+    private void createSpinAdapter()
 	{
 		adapter = ArrayAdapter.createFromResource(mContext, R.array.spinner_array, R.layout.textview);
 		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
